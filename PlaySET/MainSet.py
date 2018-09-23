7805821353a24ac0581d5aa61e3315e5086ce15d
trevor_clark <jtclark2@gmail.com>
2018-09-23 12:47:20 -0700

    Refactored main to read like a pipeline, allowing easier swapping of various underlying objects / algorithms.

--- PlaySET/MainSet.py
+++ PlaySET/MainSet.py
@@ -35,68 +35,59 @@ import CardAnalyzer
 import SetPlayer
 
 import time
-import imutils
 import cv2
-import numpy as np
 
-def display_sets_text(sets):
-    for s in sets:
-        print(s)
+
+class Game():
+
+    def process_image(self, image):
+        # Process image
+        self.ROIs = image_extractor.locate_ROIs(image)
+        # image_extractor.display_ROIs(image, ROIs, color=[255, 0, 0], line_thickness=2)
+        self.four_corner_ROIs = image_extractor.filter_for_cards(self.ROIs)
+        # image_extractor.display_ROIs(image, four_corner_ROIs, color=[255, 0, 0], line_thickness=2)
+
+        self.card_images = image_extractor.extract_images(image, self.four_corner_ROIs)
+
+        self.cards = []
+        for idx, card_image in enumerate(self.card_images):
+            card = Card.Card(index=idx, image=card_image)
+            card = card_analyzer.identify_card(card)
+            if card is not None:
+                self.cards.append(card)
+
+        self.sets = player.find_sets(self.cards)
 
 
 ##################Main######################
 if __name__ == "__main__":
     ########Select tools (extractor, analyzer, player)#########
     image_extractor = ImageExtractor.ImageExtractor()
-
     card_analyzer = CardAnalyzer.HandTunedCardAnalyzer()  # Feature analyzer is better name
-    # card_analyzer = CardAnalyzer.TemplateAnalyzer()
-
     player = SetPlayer.SetPlayer()
+    game = Game()
 
-    #IMG_PATH = "IMG_6394.JPG"
-    # IMG_PATH = "IMG_6438.JPG"
-    # IMG_PATH = "IMG_6439.JPG"
-    # IMG_PATH = "IMG_6440.JPG"
     # IMG_PATH = "IMG_6441.JPG"
     IMG_PATH = "WebCam1.PNG"
+    IMG_DIR ="ImageLibrary/%s.jpg"
+    IMG_SOURCE = "camera"
 
-    SAVE_PATH ="ImageLibrary/%s.jpg"
-
-    INPUT = "camera"
-
-if INPUT == "saved_image":
+if IMG_SOURCE == "saved_image":
     import time
     t0 = time.time()
 
     image = cv2.imread(IMG_PATH)
-    image = image_extractor.pre_process_image(IMG_PATH)
+    game.process_image(image)
 
-
-    ROIs = image_extractor.locate_ROIs(image)
-    four_corner_ROIs = image_extractor.filter_for_cards(ROIs)
-
-    card_images = image_extractor.extract_images(image, four_corner_ROIs)
-
-    cards =[]
-    for idx, card_image in enumerate(card_images):
-        card = Card.Card(index=idx, image=card_image)
-        card = card_analyzer.identify_card(card)
-        if card is not None:
-            cards.append(card)
-
-
-    sets = player.find_sets(cards)
-    # display_sets_text(sets)
-
-    image_extractor.display_cards(cards, image, ROIs, color=[255, 0, 0], line_thickness=2)
-
-    if( len(sets) > 0):
-        image_extractor.display_cards(sets[0], image, ROIs, color = [ 0, 255, 0], line_thickness = 5)
 
     print("Image processed in: %f seconds" % (time.time() - t0))
+
+    image_extractor.display_cards(game.cards, image, game.ROIs, color=[255, 0, 0], line_thickness=2)
+    if (len(game.sets) > 0):
+        image_extractor.display_cards(game.sets[0], image, game.ROIs, color=[0, 255, 0], line_thickness=5)
+
+    cv2.waitKey(0)
 else:
-    # Process from webcam (doin' it live!)
 
     cap = cv2.VideoCapture(0)
     while(True):
@@ -106,71 +97,28 @@ else:
             print("Yo! Close the previous connection fist!")
             break
 
-        ROIs = image_extractor.locate_ROIs(frame)
-        # image_extractor.display_ROIs(frame, ROIs, color=[255, 0, 0], line_thickness=2)
-        four_corner_ROIs = image_extractor.filter_for_cards(ROIs)
-        # image_extractor.display_ROIs(frame, four_corner_ROIs, color=[255, 0, 0], line_thickness=2)
+        game.process_image(frame)
 
-        card_images = image_extractor.extract_images(frame, four_corner_ROIs)
-
-        cards = []
-        for idx, card_image in enumerate(card_images):
-            card = Card.Card(index=idx, image=card_image)
-            card = card_analyzer.identify_card(card)
-            if card is not None:
-                cards.append(card)
-
-        sets = player.find_sets(cards)
-        # display_sets_text(sets)
-
-        image_extractor.display_cards(cards, frame, ROIs, color=[255, 0, 0], line_thickness=2)
-        if (len(sets) > 0):
-            image_extractor.display_cards(sets[0], frame, ROIs, color=[0, 255, 0], line_thickness=5)
+        image_extractor.display_cards(game.cards, frame, game.ROIs, color=[255, 0, 0], line_thickness=2)
+        if (len(game.sets) > 0):
+            image_extractor.display_cards(game.sets[0], frame, game.ROIs, color=[0, 255, 0], line_thickness=5)
 
 
-        ####Results
-        if(len(cards) > 0):
-            # image_extractor.display_extracted_images([card.image for card in cards])
-
-            #TODO: Heavy code duplication here
-            gray = cv2.cvtColor(cards[0].image, cv2.COLOR_BGR2GRAY)
-            blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-            ret, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-
-            contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                        cv2.CHAIN_APPROX_SIMPLE)
-            contours = contours[0] if imutils.is_cv2() else contours[1]
-
-            SYMBOL_SIZE_THRESH = 1000
-
-            contours = [c for c in contours if cv2.contourArea(c) >= SYMBOL_SIZE_THRESH]
-
-            shape = gray.shape
-            mask = np.zeros(shape, np.uint8)
-            mask = cv2.erode(mask, None, iterations=5)  # Avoid the edge...just want the texture
-
-            cv2.drawContours(mask, contours, -1, 255, -1)
 
         key_input = cv2.waitKey(1)
 
         if (key_input & 0xFF == ord('s')): # Save
             name = "ImageSet/%s.jpg" % repr(time.time())
-            cv2.imwrite(name, mask)
-
-        if (key_input & 0xFF == ord('q')): # Quit
-            break
+            cv2.imwrite(name, frame)
 
         if (key_input & 0xFF == ord('c')): # Calibrate
-        #     for()
-        # else:
-        #     #reset
-            pass
+            card = Card.Card(index=0, image=game.card_images[0])
 
-
-
+        if (key_input & 0xFF == ord('q')): # Quit
+            cap.release()
+            cv2.destroyAllWindows()
+            break
 
     # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
 
 
